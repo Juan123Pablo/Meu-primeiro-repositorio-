8ca998139b87c39a1e445974dac73250efe4bcee
Juan123Pablo <juanpablonferraz@gmail.com>
2023-04-18 21:55:30 -0300

    alterando meu nome

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000; //porta do servidor
 const server = http.createServer((req, res)=> { // Criando o servidor
     res.statusCode = 200; //Status ok de resposta
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Ola mundo uba');
+    res.end('Ola me chamo Juan Pablo, tenho 17 anos. Estou estudando desenvolvimento de sistemas no IF sudeste Campus Ubá ');
 });
 
 server.listen(port, hostname, ()=>{
